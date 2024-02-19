754c196758f4b1780d87e1bf3d7554022c594611
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 12:58:50 -0800

    Add files via upload

---
+++ Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+//written  y : TAYYABA RAO
+//DATE : 18 /FEBB /2024
+//Creating array
+let myArrray = [1, 2, 3, 4, 5];
+//accesing array by index
+console.log(myArrray[10]);
+//correct the error 
+console.log(myArrray[4]);
---
+++ Intentional_Error.ts
@@ -0,0 +1,11 @@
+//written  y : TAYYABA RAO
+//DATE : 18 /FEBB /2024
+
+//Creating array
+let myArrray :number[] = [1, 2, 3, 4, 5];
+
+//accesing array by index
+console.log(myArrray[10]);
+
+//correct the error 
+console.log(myArrray[4]);
